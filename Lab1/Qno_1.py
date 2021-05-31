aacf97be094c7a72a8f229b0587564d1290f93a8
ACER <assonshrestha99@gmail.com>
2021-05-31 09:42:23 +0545

    Second commit

--- Lab1/Qno_1.py
+++ Lab1/Qno_1.py
@@ -1,5 +1,6 @@
-a=10
-b=20
-c=25
-sum=a+b+c
-print(sum)
+num1 = int(input("the first number is: "))
+num2 = int(input("the second number is: "))
+num3 = int(input("the third number is: "))
+sum = num1+num2+num3
+print(f'the sum of the number is' ,{sum})
+
